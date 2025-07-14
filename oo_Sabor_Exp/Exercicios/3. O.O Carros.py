b0eb8388319e242ed805e487fe23e2611183703f
Ayrton Matias Borges Junior <105744107+AyrtonBorgJR@users.noreply.github.com>
2025-07-14 14:44:27 -0300

    OVD a tarde

--- oo_Sabor_Exp/Exercicios/3. O.O Carros.py
+++ oo_Sabor_Exp/Exercicios/3. O.O Carros.py
@@ -23,8 +23,13 @@ Carro.modelos()
 
 """2. Crie uma classe chamada Restaurante com os atributos nome, categoria, ativo e crie mais 2 atributos.
 Instancie um restaurante e atribua valores aos seus atributos.
+
 3. Modifique a classe Restaurante adicionando um construtor que aceita nome e categoria como parâmetros
-e inicia ativo como False por padrão. Crie uma instância utilizando o construtor."""
+e inicia ativo como False por padrão. Crie uma instância utilizando o construtor.
+
+4. Adicione um método especial __str__ à classe Restaurante para que, ao imprimir uma instância,
+seja exibida uma mensagem formatada com o nome e a categoria. Exiba essa mensagem para uma
+instância de restaurante."""
 
 class Restaurante():
     estabelecimentos = []
@@ -57,11 +62,33 @@ r1 = Restaurante(nome, categoria, ativo, avaliacao, bairro)
 
 Restaurante.listar_restaurantes()
 
-
-
-"""4. Adicione um método especial __str__ à classe Restaurante para que, ao imprimir uma instância,
-seja exibida uma mensagem formatada com o nome e a categoria. Exiba essa mensagem para uma
-instância de restaurante."""
-
 """5. Crie uma classe chamada Cliente e pense em 4 atributos. Em seguida, instancie 3 objetos
 desta classe e atribua valores aos seus atributos através de um método construtor."""
+
+class cliente():
+    lista_clientes = []
+
+    def __init__(self, nome, idade, bairro, perfil):
+        self.nome = nome
+        self.idade = idade
+        self.bairro = bairro
+        self.perfil = perfil
+        cliente.lista_clientes.append(self)
+    
+    def __str__(self):
+        return f'{self.nome} | {self.idade} | {self.bairro} | {self.perfil}'
+
+    def listagem():
+        print(f'{"Nome":<15} | {"Idade":<15} | {"Bairro":<20} | {"Perfil":<15}')
+        for pessoas in cliente.lista_clientes:
+            print(f'{pessoas.nome:<15} | {pessoas.idade:<15} | {pessoas.bairro:<20} | {pessoas.perfil:<15}')
+
+
+nome = "Antonio"
+idade = "81"
+bairro = "Silveira da Motta"
+perfil = "Consumidor Médio"
+
+cliente1 = cliente(nome, idade, bairro, perfil)
+
+cliente.listagem()
