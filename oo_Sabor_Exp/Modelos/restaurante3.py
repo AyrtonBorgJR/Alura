52189a8a5a27d25429b31c7fea3831537179fcbf
Ayrton Matias Borges Junior <105744107+AyrtonBorgJR@users.noreply.github.com>
2025-07-24 14:09:13 -0300

    tarde.

---
+++ oo_Sabor_Exp/Modelos/restaurante3.py
@@ -0,0 +1,25 @@
+class Restaurante():
+    estabelecimentos = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title() #Titulo
+        self._categoria = categoria.upper() #Tudo em MAIUSCULO
+        self._ativo = False
+        Restaurante.estabelecimentos.append(self)
+
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
+
+    @classmethod
+    def lista_restaurantes(cls):
+        print(f'{"Restaurante":<25} | {"Categoria":<10} | {"Status":<6}' )
+        for restaurante in cls.estabelecimentos:
+            print(f'{restaurante._nome:<25} | {restaurante._categoria:<10} | {restaurante.ativo:<6}')
+
+    @property
+    def ativo(self):
+        return "✓" if self._ativo else "⌧"
+
+    def alt_status(self):
+        self._ativo = not self._ativo
+
---
+++ oo_Sabor_Exp/app.py
@@ -0,0 +1,12 @@
+from Modelos.restaurante3 import Restaurante
+
+restaunrante_bom = Restaurante('praca', 'Gourmet')
+restaurante_ruim = Restaurante('MexFood', 'Mexicano')
+restaurante_3 = Restaurante('Japa', 'Japones')
+
+
+def main:
+    Restaurante.listar_restaurante
+
+if __name__ == '__main__':
+    main()
